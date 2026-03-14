527662fdd394cd242485f3bce8d3e24f027b2ff3
deekshi275 <deekshithdee018@gmail.com>
2026-03-14 21:30:57 +0530

    day16

---
+++ day16.js
@@ -0,0 +1,14 @@
+ let a = 3
+let prim = true
+for (let i =2;i<=Math.floor(Math.sqrt(a));i++){
+    if (a%i==0){
+        prim = false
+    }
+}if (prim){
+    console.log('prime number')
+
+}else{
+    console.log('not a prime number')
+}
+
+
